85efd62d5b39b7af41d1fe9604bffeb20b932b01
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-21 15:04:18 +0530

    new

---
+++ src/test/java/com/api_RestAssure/FirstApiTest.java
@@ -0,0 +1,14 @@
+package com.api_RestAssure;
+
+import org.testng.annotations.Test;
+
+public class FirstApiTest {
+	
+	
+	@Test
+	public void testApi()
+	{
+		System.out.println("Api First Demo");
+	}
+
+}
