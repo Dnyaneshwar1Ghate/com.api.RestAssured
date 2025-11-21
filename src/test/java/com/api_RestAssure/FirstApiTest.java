e6eda13cb915e92797f71903d63fa05196184b47
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-21 16:40:30 +0530

    new change

--- src/test/java/com/api_RestAssure/FirstApiTest.java
+++ src/test/java/com/api_RestAssure/FirstApiTest.java
@@ -9,9 +9,10 @@ public class FirstApiTest {
 	@Test
 	public void demo_GEt_Request() {
 		given().when().get("https://reqres.in/api/users?page=2").then().statusCode(200).statusLine("HTTP/1.1 200 OK")
-				// Corrected JSON path to access 'email' within 'data' array
+
 				.body("data[0].email", equalTo("michael.lawson@reqres.in")).header("Server", "cloudflare");
 
+		System.out.println("First Test Case Pass for API Automation using RestAssured");
 	}
 
 }
