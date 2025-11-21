0a6a44227becaf98876a00c1f6336620198dbb49
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-21 16:37:54 +0530

    new change

--- src/test/java/com/api_RestAssure/FirstApiTest.java
+++ src/test/java/com/api_RestAssure/FirstApiTest.java
@@ -1,14 +1,17 @@
 package com.api_RestAssure;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
 public class FirstApiTest {
-	
-	
+
 	@Test
-	public void testApi()
-	{
-		System.out.println("Api First Demo");
+	public void demo_GEt_Request() {
+		given().when().get("https://reqres.in/api/users?page=2").then().statusCode(200).statusLine("HTTP/1.1 200 OK")
+				// Corrected JSON path to access 'email' within 'data' array
+				.body("data[0].email", equalTo("michael.lawson@reqres.in")).header("Server", "cloudflare");
+
 	}
 
 }
