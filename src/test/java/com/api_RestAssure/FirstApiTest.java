d473869154367c6af69b993325276d20c7255077
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-24 15:08:34 +0530

    my own api testing

--- src/test/java/com/api_RestAssure/FirstApiTest.java
+++ src/test/java/com/api_RestAssure/FirstApiTest.java
@@ -9,10 +9,10 @@ public class FirstApiTest {
 	@Test
 	public void demo_GEt_Request() {
 		
-		given().when().get("https://jsonplaceholder.typicode.com/posts").then().statusCode(200).statusLine("HTTP/1.1 200 OK")
+		given().when().get("http://localhost:3000/employees").then().statusCode(200).statusLine("HTTP/1.1 200 OK")
 
 		.assertThat()		
-		.body("title[1]", equalTo("qui est esse")).header("Content-Type", "application/json; charset=utf-8");
+		.body("firstName[4]", equalTo("mahesh")).header("Content-Type", "application/json");
 
 		System.out.println("First Test Case Pass for GEt API Automation using RestAssured Done");
 	}
