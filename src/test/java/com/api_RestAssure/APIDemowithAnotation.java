54238b2081e837ac498b2904e36e07581c5ea872
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-27 18:52:04 +0530

    new

---
+++ src/test/java/com/api_RestAssure/APIDemowithAnotation.java
@@ -0,0 +1,44 @@
+package com.api_RestAssure;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+
+import static io.restassured.RestAssured.*;
+public class APIDemowithAnotation {
+	
+	@BeforeTest
+
+    public void setup() {
+
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+
+    }
+
+    @Test
+
+    public void validateGetRequest() {
+
+        given()
+
+            .when().get("/posts/1")
+
+            .then().statusCode(200)
+
+            .log().all();
+
+    }
+
+    @AfterTest
+
+    public void teardown() {
+
+        System.out.println("Test completed.");
+
+    }
+
+
+}
