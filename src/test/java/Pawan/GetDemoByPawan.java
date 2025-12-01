0943104ee4c1e2f0017f3316e83829c4c50c5e2a
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-12-01 18:36:33 +0530

    new changes

--- src/test/java/Pawan/GetDemoByPawan.java
+++ src/test/java/Pawan/GetDemoByPawan.java
@@ -1,13 +1,26 @@
 package Pawan;
 
 import org.testng.annotations.Test;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class GetDemoByPawan {
 	
-	@Test
+	@Test()
 	public void getRequest()
 	{
-		System.out.println("hello by pawan");
+		given()
+		.when()
+		.get("https://api.restful-api.dev/objects")
+		.then()
+		.statusCode(200)
+		.statusLine("HTTP/1.1 200 OK")
+		.assertThat().body("name[0]", equalTo("Google Pixel 6 Pro"))
+		.header("Content-Type", "application/json");
+		
+		
 	}
 
 }
---
+++ src/test/java/Pawan/TestListener.java
@@ -0,0 +1,16 @@
+package Pawan;
+
+import org.testng.ITestListener;
+import org.testng.annotations.Test;
+
+public class TestListener implements ITestListener {
+	
+		@Test
+		public void mainTEst()
+		{
+			
+			
+		}
+	}
+
+
